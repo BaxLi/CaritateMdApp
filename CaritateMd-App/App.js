73fd081f8169b9889b70e5691e90c8b8d3b241c0
BaxLi <lilian.baxan@gmail.com>
2019-12-16 12:08:55 +0200

    temp-ok-1

--- CaritateMd-App/App.js
+++ CaritateMd-App/App.js
@@ -1,9 +1,13 @@
-import React from 'react';
+/* eslint-disable no-unused-vars */
+import React, {Component} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 export default function App() {
   return (
     <View style={styles.container}>
+       <FontAwesomeIcon icon={ faCoffee } />
       <Text>Open up App.js to start working on your app!</Text>
     </View>
   );
@@ -14,6 +18,7 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-  },
+    justifyContent: 'center'
+  }
+// eslint-disable-next-line comma-dangle
 });
---
+++ CaritateMd-App/Readme.txt
@@ -0,0 +1,4 @@
+//check to add if free icons is ok   
+$ npm i --save @fortawesome/free-brands-svg-icons
+$ npm i --save @fortawesome/free-regular-svg-icons
+
